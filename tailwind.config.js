293df28770afcdcb282da9deafac4224a0c466a1
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-09-14 11:38:05 -0300

    feat(theme): add color palette and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ice: "#F7FDFC",
+        cyan: "#38CED6",
+        blue: "#389CD6",
+        green: "#38D673",
+        navy: "#2A50A1",
+        red: "#D6453F",
+      },
+      fontFamily: {
+        sans: ["Nunito Sans", "sans-serif"],
+        dancing: ["Dancing Script", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
